280578e888a6f1c1617a7e0a5f88cf6a1f01b866
zxfjd3g <258147149@qq.com>
2017-12-05 14:37:20 +0800

    init app

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ele-header></ele-header>
+
+    <div class="tab">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评价</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header.vue'
+  export default {
+    components: {
+      'ele-header': header
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .tab
+    .tab-item
+      display inline-block
+      .router-link-active
+        color red
+</style>
